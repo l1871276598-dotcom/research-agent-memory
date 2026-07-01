ee59c1eea3dddb41a5b95dc375ba135c14593c7f
l1871276598-dotcom <l1871276598@gmail.com>
2026-07-01 21:28:57 +0800

    export MCP checkpoint capture service

--- src/bridge/__init__.py
+++ src/bridge/__init__.py
@@ -1,5 +1,6 @@
 from .factory import build_bridge_pipeline
 from .inbox import BridgeEventInbox
+from .mcp_checkpoint import McpCheckpointCapture
 from .projector import BridgeScopeResolver, SessionProjector
 from .recovery import recover_processing
 from .service import BridgeIngestService, BridgePipeline
@@ -9,6 +10,7 @@ __all__ = [
     "BridgeIngestService",
     "BridgePipeline",
     "BridgeScopeResolver",
+    "McpCheckpointCapture",
     "SessionProjector",
     "build_bridge_pipeline",
     "recover_processing",
