8615d180056aee5b8b0953ca06a84d90480a8fe9
l1871276598-dotcom <l1871276598@gmail.com>
2026-07-01 18:43:13 +0800

    export message transport interfaces

--- src/message_io/__init__.py
+++ src/message_io/__init__.py
@@ -1,1 +1,15 @@
-"""LAOS message transport abstractions."""
+from .adapters import AdapterRegistry, BaseAdapter, CallbackAdapter
+from .router import AccessPolicy, GatewayRouter
+from .types import MessageEvent, MessageSource, split_message, utf16_len
+
+__all__ = [
+    "AccessPolicy",
+    "AdapterRegistry",
+    "BaseAdapter",
+    "CallbackAdapter",
+    "GatewayRouter",
+    "MessageEvent",
+    "MessageSource",
+    "split_message",
+    "utf16_len",
+]
