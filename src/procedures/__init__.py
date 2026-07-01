32f7d3f47b34440c73db5a9f8728dce82e94f0ba
l1871276598-dotcom <l1871276598@gmail.com>
2026-07-01 18:44:08 +0800

    export procedure lifecycle services

--- src/procedures/__init__.py
+++ src/procedures/__init__.py
@@ -1,1 +1,17 @@
-"""Reusable task procedures for LAOS."""
+from .catalog import ProcedureCatalog
+from .curator_apply import ProcedureCuratorTransitions
+from .curator_plan import build_plan, target_state
+from .curator_runner import ProcedureCurator
+from .manager import ProcedureManager
+from .proposals import ProcedureProposalStore, validate_proposal
+
+__all__ = [
+    "ProcedureCatalog",
+    "ProcedureCurator",
+    "ProcedureCuratorTransitions",
+    "ProcedureManager",
+    "ProcedureProposalStore",
+    "build_plan",
+    "target_state",
+    "validate_proposal",
+]
