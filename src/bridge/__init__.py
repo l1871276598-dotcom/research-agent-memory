07050500488976ecea32e26015a765764d2745ae
l1871276598-dotcom <l1871276598@gmail.com>
2026-07-01 20:45:43 +0800

    export bridge recovery helper

--- src/bridge/__init__.py
+++ src/bridge/__init__.py
@@ -1,6 +1,7 @@
 from .factory import build_bridge_pipeline
 from .inbox import BridgeEventInbox
 from .projector import BridgeScopeResolver, SessionProjector
+from .recovery import recover_processing
 from .service import BridgeIngestService, BridgePipeline
 
 __all__ = [
@@ -10,4 +11,5 @@ __all__ = [
     "BridgeScopeResolver",
     "SessionProjector",
     "build_bridge_pipeline",
+    "recover_processing",
 ]
